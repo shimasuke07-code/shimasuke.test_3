45dc173ce701957d633998a0876bbc7e3bec1685
shimasuke07-code <shimasuke07@gmail.com>
2025-12-10 13:38:41 +0900

    Initialize script.js with task management functions

---
+++ script.js
@@ -0,0 +1,11 @@
+let tasks = [];
+let points = 0;
+let level = 1;
+let exp = 0;
+
+function addTask() { ... }
+function toggleDone(index) { ... }
+function renderTasks() { ... }
+function updateSidebar() { ... }
+function toggleSidebar() { ... }
+function updateAvatarAppearance() { ... }
